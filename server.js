447740aacd4e5c6db8c2e04e6801619159b67375
QuangDev <ng.quang.coder.new@gmail.com>
2024-10-04 10:36:50 +0700

    Init Project and install packets

---
+++ server.js
@@ -0,0 +1,13 @@
+// Start server
+const app = require("./src/app");
+
+const PORT = 3050;
+
+const server = app.listen(PORT, () => {
+  console.log(`WSV eCommerce start with ${PORT}`);
+});
+
+// event when Ctrl C (Before wxits app)
+process.on("SIGINT", () => {
+  server.close(() => console.log(`Exit Server Express`));
+});
---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+const app = express();
+
+// init middlewares
+// Morgan Options [dev,combined,common,short,tiny]
+app.use(morgan("dev"));
+// Helmet (hide Header information)
+app.use(helmet());
+// Compression (save bandwidth)
+app.use(compression());
+// init database
+
+// init routes
+app.get("/", (req, res, next) => {
+  const str = "Hello Bro";
+  return res.status(200).json({
+    message: "Welcome FanTipJs!",
+    metadata: str.repeat(10000000),
+  });
+});
+
+// handling errors
+module.exports = app;
